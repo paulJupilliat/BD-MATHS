3e643b80fde763833e3afea5e5afc57ec42ae774
JingerDwarf <antoine.saada25@gmail.com>
2023-03-29 11:33:53 +0200

    v1

--- creation.sql
+++ creation.sql
@@ -1,90 +1,31 @@
--- Active: 1678109077661@@127.0.0.1@3306@SAE_BD
+DROP TABLE IF EXISTS Vol;
+DROP TABLE IF EXISTS Aéroport;
+DROP TABLE IF EXISTS CompagnieAérienne;
 
-drop table VOLS;
+CREATE TABLE CompagnieAérienne (
+  compagnie_id INT PRIMARY KEY,
+  nom VARCHAR(255) NOT NULL
+);
 
-drop table TERMINAUX;
+CREATE TABLE Aéroport (
+  aeroport_id INT PRIMARY KEY,
+  nom VARCHAR(255) NOT NULL UNIQUE,
+  ville VARCHAR(255) NOT NULL,
+  pays VARCHAR(255) NOT NULL,
+  terminal VARCHAR(255) NOT NULL
+);
 
-DROP TABLE ESCALE;
-
-drop table VOL_GENERIQUE;
-
-
-drop table AEROPORT;
-
-drop table VILLES;
-
-drop table COMPAGNIE;
-
-create table
-    COMPAGNIE(
-        id_compagnie int not null auto_increment,
-        nom_compagnie varchar(50) not null,
-        PRIMARY KEY (id_compagnie)
-    );
-
-create table
-    VILLES(
-        nom_ville varchar(50) not null,
-        pays_ville varchar(50) not null,
-        PRIMARY KEY (nom_ville, pays_ville)
-    );
-
-create table
-    AEROPORT(
-        id_aeroport int not null auto_increment,
-        nom_aeroport varchar(50) not null,
-        ville VARCHAR(50) not null,
-        PRIMARY KEY (id_aeroport),
-        FOREIGN KEY (ville) REFERENCES VILLES(nom_ville)
-    );
-create table
-    ESCALE(
-        id_aeroport int not null,
-        date_arrivee varchar(50) not null,
-        date_depart varchar(50) not null,
-        PRIMARY KEY (id_aeroport),
-        FOREIGN KEY (id_aeroport) REFERENCES AEROPORT(id_aeroport)
-    );
-
-CREATE table
-    VOL_GENERIQUE(
-        id_vol_generique int not null auto_increment,
-        aeroport_depart int not null,
-        esclale int DEFAULT nullnot nul,
-        aeroport_arrivee int not null,
-        jour varchar(50) not null,
-        heure_depart varchar(50) not null,
-        heure_arrivee varchar(50) not null,
-        duree varchar(50) not null,
-        PRIMARY KEY (id_vol_generique),
-        FOREIGN KEY (aeroport_depart) REFERENCES AEROPORT(id_aeroport),
-        FOREIGN KEY (aeroport_arrivee) REFERENCES AEROPORT(id_aeroport),
-        FOREIGN KEY (esclale) REFERENCES ESCALE(id_aeroport)
-    );
-
-
-
-create table
-    TERMINAUX(
-        id_terminal int not null auto_increment,
-        id_aeroport int not null,
-        nom_terminal varchar(50) not null,
-        PRIMARY KEY (id_terminal, id_aeroport),
-        FOREIGN KEY (id_aeroport) REFERENCES AEROPORT(id_aeroport)
-    );
-
-create table
-    VOLS(
-        id_vols int not null auto_increment,
-        date_arrivee varchar(50) not null,
-        date_depart varchar(50) not null,
-        id_vol_generique int not null,
-        id_compagnie int not null,
-        terminal_depart int not null,
-        terminal_arrivee int not null,
-        PRIMARY KEY (id_vols),
-        FOREIGN KEY (id_vol_generique) REFERENCES VOL_GENERIQUE(id_vol_generique),
-        FOREIGN KEY (id_compagnie) REFERENCES COMPAGNIE(id_compagnie),
-        FOREIGN KEY (terminal_depart) REFERENCES TERMINAUX(id_terminal),
-        FOREIGN KEY (terminal_arrivee) REFERENCES TERMINAUX(id_terminal)
-    );
+CREATE TABLE Vol (
+  vol_id INT PRIMARY KEY,
+  compagnie_id INT,
+  numero INT NOT NULL,
+  date_depart DATE NOT NULL,
+  heure_depart TIME NOT NULL,
+  aeroport_depart_id INT,
+  date_arrivee DATE NOT NULL,
+  heure_arrivee TIME NOT NULL,
+  aeroport_arrivee_id INT,
+  FOREIGN KEY (compagnie_id) REFERENCES CompagnieAérienne(compagnie_id),
+  FOREIGN KEY (aeroport_depart_id) REFERENCES Aéroport(aeroport_id),
+  FOREIGN KEY (aeroport_arrivee_id) REFERENCES Aéroport(aeroport_id)
+);
